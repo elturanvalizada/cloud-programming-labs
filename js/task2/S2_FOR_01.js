9581ca3a3d08c61392df4b1dfa24b5de8a8a47a0
Elturan Valizada <elturanvalizada@Elturans-MacBook-Pro.local>
2026-01-31 19:53:02 +0400

    TASK1: S2_FOR_01 - FizzBuzz using for loop

---
+++ js/task2/S2_FOR_01.js
@@ -0,0 +1,19 @@
+// S2_FOR_01 — FizzBuzz+
+
+function fizzBuzz(N) {
+  for (let i = 1; i <= N; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+// Test
+fizzBuzz(15);
+
